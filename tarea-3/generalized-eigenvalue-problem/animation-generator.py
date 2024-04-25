565ed4962eed0f03fa9a85e5b3b1ec9b1f0fb231
th <th.martinod@gmail.com>
2024-04-25 15:47:22 -0500

    summary (required)

--- tarea-3/generalized-eigenvalue-problem/animation-generator.py
+++ tarea-3/generalized-eigenvalue-problem/animation-generator.py
@@ -1,25 +1,55 @@
-from manim import *
+from manimlib.imports import *
 
-class LinearTransformationSceneExample(LinearTransformationScene):
-    def __init__(self, **kwargs):
-        LinearTransformationScene.__init__(
-            self,
-            show_coordinates=True,
-            leave_ghost_vectors=True,
-            *kwargs
-        )
+class Vectors(VectorScene):
+    CONFIG = {
+        "show_basis_vectors" : True,
+        "foreground_plane_kwargs" : {
+            "x_max" : 10,
+            "x_min" : -10,
+            "y_max" : 10,
+            "y_min" : -10,
+        },
+    }
 
     def construct(self):
-        # Ask the user for the matrix input
-        matrix_str = input("Enter the elements of the 2x2 matrix separated by spaces: ")
-        matrix_elements = [float(x) for x in matrix_str.split()]
-        matrix = [[matrix_elements[0], matrix_elements[1]], 
-                  [matrix_elements[2], matrix_elements[3]]]
-
-        # Apply the user-defined matrix
-        self.apply_matrix(matrix)
+        self.add_axes(animate = False)
+        self.add_plane(animate = False)
         self.wait()
 
-# Run the scene
-scene = LinearTransformationSceneExample()
-scene.render()
+        myVec = Vector([3,2])
+        myVec.set_color(YELLOW)
+        self.add_vector(myVec)
+
+        labelVector = vector_coordinate_label(myVec, color = YELLOW)
+        self.play(ShowCreation(labelVector))
+
+
+
+class LinearTransformation(LinearTransformationScene):
+    CONFIG = {
+        "show_basis_vectors" : True,
+        "foreground_plane_kwargs" : {
+            "x_max" : 10,
+            "x_min" : -10,
+            "y_max" : 10,
+            "y_min" : -10,
+            "faded_line_ratio" : 0
+        },
+    }
+    
+    def construct(self):
+        self.setup()
+        self.wait()
+
+        myVec = Vector([3,2])
+        myVec.set_color(YELLOW)
+        self.add_vector(myVec)
+
+        labelVector = vector_coordinate_label(myVec, color = YELLOW)
+        self.add_moving_mobject(myVec)
+
+        text = TexMobject("Shear")
+        text.scale(2)
+        self.add_transformable_mobject(text)
+
+        self.apply_matrix([[2,-1], [3,-5]])
